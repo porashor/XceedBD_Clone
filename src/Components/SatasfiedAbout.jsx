8f8ff1678875974d296567948e9c906c86c02fce
Parashar Das <porashorbabu@gmail.com>
2025-03-20 23:49:56 +0600

    about

---
+++ src/Components/SatasfiedAbout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SatasfiedAbout = () => {
+  return (
+    <div className='text-white bg-black py-10'>
+      {/* container section  */}
+      <div className='w-[90%] mx-auto'>
+        {/* header section  */}
+        <div className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl  text-center '>Our clients love working with us
+        </div>
+        <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold  text-center py-5 '>We are masters of industry</h1>
+        {/* details section  */}
+        <div className='text-3xl md:text-4xl lg:text-5xl font-bold'></div>
+      </div>
+    </div>
+  )
+}
+
+export default SatasfiedAbout
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import TopAbout from '../Components/TopAbout'
+import SatasfiedAbout from '../Components/SatasfiedAbout'
 const About = () => {
   return (
     <div>
       <TopAbout/>
+      <SatasfiedAbout/>
     </div>
   )
 }
